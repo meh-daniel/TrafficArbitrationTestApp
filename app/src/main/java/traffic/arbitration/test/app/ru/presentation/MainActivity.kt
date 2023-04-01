b115d8eaf3b2b3013919d2604ddc323800c9b149
daniel <meh.daniel@yandex.com>
2023-04-01 12:15:41 +0700

    change activity

---
+++ app/src/main/java/traffic/arbitration/test/app/ru/presentation/MainActivity.kt
@@ -0,0 +1,6 @@
+package traffic.arbitration.test.app.ru.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import traffic.arbitration.test.app.ru.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
